6082b8d759ce92ff0e7151a265d75129721ab125
leroysb <leroybuliro@gmail.com>
2023-08-30 14:12:28 +0300

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ module 0-island_perimeter contains function island_perimeter """
+
+
 def island_perimeter(grid):
     """ island_perimeter returns the perimeter of the island described in grid
+
+    Args:
+        grid (list): a list of lists of integers:
+            0 represents water
+            1 represents land
+            Each cell is square, with a side length of 1
+            Cells are connected horizontally/vertically (not diagonally).
+            grid is rectangular, with its width and height not exceeding 100
+    Returns:
+        int: the perimeter of the island described in grid
     """
     perimeter = 0
     for row in range(len(grid)):
