447a10cad73c0ff97330518daf52f83edbb81e2f
LeroySBN <leroybuliro@gmail.com>
2025-07-11 20:57:00 +0300

    fix mixup

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
+"""pascal_triangle.py
 """
-module pascal_triangle
-"""
 
 
-def canUnlockAll(boxes):
-    visited = [False] * len(boxes)
-    visited[0] = True
-    return unlockBoxes(0, boxes, visited)
+def pascal_triangle(n):
+    my_list = list()
+    my_list_joined = list()
 
+    if n <= 0:
+        return my_list
 
-def unlockBoxes(box, boxes, visited):
-    if all(visited):
-        return True
+    my_list.append(1)
+    
+    for i in range(n):
+        if i > 0:
+            my_list.append(0)
+            new_list = list.copy(my_list)
+            for j in range(1,i+1):
+                new_list[j] = my_list[j] + my_list[j-1]
+            my_list.clear()
+            my_list = list.copy(new_list)
+        my_list_joined.append(list(my_list))
+        # print(my_list_joined)
+    return my_list_joined
 
-    keys = boxes[box]
-    for key in keys:
-        if key < len(boxes) and not visited[key]:
-            visited[key] = True
-            if unlockBoxes(key, boxes, visited):
-                return True
-
-    return False
+pascal_triangle(5)
