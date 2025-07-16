90a8604dbc278858b9a5afd035123c53b06edb8f
LeroySBN <leroybuliro@gmail.com>
2025-07-16 04:47:38 +0300

    reattempt

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,41 +1,18 @@
 #!/usr/bin/python3
+"""0-lockboxes.py
 """
-module 0-lockboxes
-"""
-
 
 def canUnlockAll(boxes):
-    """
-    Determines if all the boxes in a list of boxes can be opened
-    """
-    # Create a list to track visited boxes, initially all set to False
-    visited = [False] * len(boxes)
-    # Mark the first box as visited (the starting box)
-    visited[0] = True
-    # Call the helper function to explore and unlock boxes
-    return unlockBoxes(0, boxes, visited)
+    unlocked = [False] * len(boxes)
+    unlocked[0] = True
+    return unlockRecursive(0, boxes, unlocked)
 
-
-def unlockBoxes(box, boxes, visited):
-    """
-    Helper function to recursively explore and unlock boxes
-    """
-    if all(visited):
-        # If all boxes visited, return True to indicate all boxes can be opened
+def unlockRecursive(idx, boxes, unlocked):
+    if all(unlocked):
         return True
-
-    keys = boxes[box]  # Get the keys in the current box
-    for key in keys:
-        if key < len(boxes) and not visited[key]:
-            # if key is within range and
-            # the box corresponding to the key has not been visited
-            visited[key] = True  # Mark the box as visited
-            if unlockBoxes(key, boxes, visited):
-                # Recursively call the function with
-                # the key as the new starting box index
-                # If the recursive call returns True,
-                # return True to propagate the result
+    for key in boxes[idx]:
+        if key < len(boxes) and not unlocked[key]:
+            unlocked[key] = True
+            if unlockRecursive(key, boxes, unlocked):
                 return True
-
-    # If no key allows all boxes to be opened, return False
     return False
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -3,28 +3,28 @@
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
 boxes = [[1], [2], [3], [4], []] # True
-print(canUnlockAll(boxes))
+print(f"boxList 1 (Expected - True) -> {canUnlockAll(boxes)}")
 
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]] # True
-print(canUnlockAll(boxes))
+print(f"boxList 2 (Expected - True) -> {canUnlockAll(boxes)}")
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]] # False
-print(canUnlockAll(boxes))
+print(f"boxList 3 (Expected - False) -> {canUnlockAll(boxes)}")
 
 boxes = [[], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]] # False
-print(canUnlockAll(boxes))
+print(f"boxList 4 (Expected - False) -> {canUnlockAll(boxes)}")
 
 boxes = [[0], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]] # False
-print(canUnlockAll(boxes))
+print(f"boxList 5 (Expected - False) -> {canUnlockAll(boxes)}")
 
 boxes = [[7], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]  # False
-print(canUnlockAll(boxes))
+print(f"boxList 6 (Expected - False) -> {canUnlockAll(boxes)}")
 
 boxes = [[1, 3, 7], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]] # True
-print(canUnlockAll(boxes))
+print(f"boxList 7 (Expected - True) -> {canUnlockAll(boxes)}")
 
 boxes = [[]]
-print(canUnlockAll(boxes))  # True
+print(f"boxList 8 (Expected - True) -> {canUnlockAll(boxes)}")  # True
 
 boxes = [[1], [0, 2], [3]] # True
-print(canUnlockAll(boxes))
+print(f"boxList 9 (Expected - True) -> {canUnlockAll(boxes)}")  # True
